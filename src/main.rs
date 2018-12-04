3f124937cd43cde78947e19ec80383a6caf03477
palmern <palmern@lmax.com>
2018-12-04 13:48:15 +0000

    2018 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod aoc2017;
+mod aoc2018;
 
 fn main() {
     aoc2017();
+    aoc2018();
 }
 
 fn aoc2017() {
@@ -16,5 +18,16 @@ fn aoc2017() {
         "\tDay 2: Part One={}, Part Two={}",
         aoc2017::day2::solve_part_one(aoc2017::DAY2_INPUT),
         aoc2017::day2::solve_part_two(aoc2017::DAY2_INPUT),
+    );
+}
+
+
+fn aoc2018() {
+    println!("Advent of Code 2018");
+
+    println!(
+        "\tDay 1: Part One={}, Part Two={}",
+        aoc2018::day1::solve_part_one(aoc2018::DAY1_INPUT),
+        aoc2018::day1::solve_part_two(aoc2018::DAY1_INPUT),
     );
 }
