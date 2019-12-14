8c8203d7e53762f856306bbfad58e90cf3014c79
palmern <palmern@lmax.com>
2019-12-14 09:50:34 +0000

    fmt

--- src/aoc2019/mod.rs
+++ src/aoc2019/mod.rs
@@ -6,11 +6,14 @@ mod day3;
 pub fn aoc2019() {
     println!("Advent of Code 2019");
 
-    //    println!(
-    //        "\tDay 2: Part One={}, Part Two={}",
-    //        day2::solve_part_one(&util::read_file_input("resources/2019/day2.txt"), day2::part_1_mangling),
-    //        day2::solve_part_two(&util::read_file_input("resources/2019/day2.txt")),
-    //    );
+    println!(
+        "\tDay 2: Part One={}, Part Two={}",
+        day2::solve_part_one(
+            &util::read_file_input("resources/2019/day2.txt"),
+            day2::part_1_mangling
+        ),
+        day2::solve_part_two(&util::read_file_input("resources/2019/day2.txt")),
+    );
 
     println!(
         "\tDay 3: Part One={}, Part Two={}",
