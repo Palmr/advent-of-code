0686a3c065b92fb9a3eabe03cb27ef964342b373
Nick Palmer <nick@palmr.co.uk>
2020-12-06 17:03:30 +0000

    Add Simon-solution to day5

--- src/aoc2020/day5.rs
+++ src/aoc2020/day5.rs
@@ -1,4 +1,4 @@
-/// --- Day 5: Binary Boarding ---
+/// # Day 5: Binary Boarding
 ///
 /// You board your plane only to discover a new problem: you dropped your boarding pass! You aren't
 /// sure which seat is yours, and all of the flight attendants are busy with the flood of people
@@ -8,17 +8,18 @@
 /// (your puzzle input); perhaps you can find your seat through process of elimination.
 ///
 /// Instead of zones or groups, this airline uses binary space partitioning to seat people. A seat
-/// might be specified like FBFBBFFRLR, where F means "front", B means "back", L means "left", and
-/// R means "right".
+/// might be specified like `FBFBBFFRLR`, where `F` means "front", `B` means "back", `L` means "
+/// left", and `R` means "right".
 ///
-/// The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the
-/// plane (numbered 0 through 127). Each letter tells you which half of a region the given seat is
-/// in. Start with the whole list of rows; the first letter indicates whether the seat is in the
+/// The first 7 characters will either be `F` or `B`; these specify exactly one of the 128 rows on
+/// the plane (numbered 0 through 127). Each letter tells you which half of a region the given seat
+/// is in. Start with the whole list of rows; the first letter indicates whether the seat is in the
 /// front (0 through 63) or the back (64 through 127). The next letter indicates which half of that
 /// region the seat is in, and so on until you're left with exactly one row.
 ///
-/// For example, consider just the first seven characters of FBFBBFFRLR:
+/// For example, consider just the first seven characters of `FBFBBFFRLR`:
 ///
+/// ```
 ///     Start by considering the whole range, rows 0 through 127.
 ///     F means to take the lower half, keeping rows 0 through 63.
 ///     B means to take the upper half, keeping rows 32 through 63.
@@ -27,33 +28,39 @@
 ///     B keeps rows 44 through 47.
 ///     F keeps rows 44 through 45.
 ///     The final F keeps the lower of the two, row 44.
+/// ```
 ///
-/// The last three characters will be either L or R; these specify exactly one of the 8 columns of
-/// seats on the plane (numbered 0 through 7). The same process as above proceeds again, this time
-/// with only three steps. L means to keep the lower half, while R means to keep the upper half.
+/// The last three characters will be either `L` or `R`; these specify exactly one of the 8 columns
+/// of seats on the plane (numbered 0 through 7). The same process as above proceeds again, this
+/// time with only three steps. `L` means to keep the lower half, while `R` means to keep the upper
+/// half.
 ///
-/// For example, consider just the last 3 characters of FBFBBFFRLR:
+/// For example, consider just the last 3 characters of `FBFBBFFRLR`:
 ///
+/// ```
 ///     Start by considering the whole range, columns 0 through 7.
 ///     R means to take the upper half, keeping columns 4 through 7.
 ///     L means to take the lower half, keeping columns 4 through 5.
 ///     The final R keeps the upper of the two, column 5.
+/// ```
 ///
-/// So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
+/// So, decoding `FBFBBFFRLR` reveals that it is the seat at row 44, column 5.
 ///
 /// Every seat also has a unique seat ID: multiply the row by 8, then add the column. In this
-/// example, the seat has ID 44 * 8 + 5 = 357.
+/// example, the seat has ID `44 * 8 + 5 = 357`.
 ///
 /// Here are some other boarding passes:
 ///
+/// ```
 ///     BFFFBBFRRR: row 70, column 7, seat ID 567.
 ///     FFFBBBFRRR: row 14, column 7, seat ID 119.
 ///     BBFFBBFRLL: row 102, column 4, seat ID 820.
+/// ```
 ///
 /// As a sanity check, look through your list of boarding passes. What is the highest seat ID on a
 /// boarding pass?
 ///
-/// --- Part Two ---
+/// ## Part Two
 ///
 /// Ding! The "fasten seat belt" signs have turned on. Time to find your seat.
 ///
@@ -66,6 +73,9 @@
 ///
 /// What is the ID of your seat?
 
+/// This was my initial naive solution, taking the break down explanation of how the row/columns are
+/// calculated and turning it in to code.
+#[allow(dead_code)]
 fn binary_space_partition(input: &str, value_range: (usize, usize)) -> usize {
     let range = input.chars().fold(value_range, |range, c| {
         let half = (range.1 - range.0) / 2;
@@ -80,9 +90,30 @@ fn binary_space_partition(input: &str, value_range: (usize, usize)) -> usize {
     range.0
 }
 
+/// This is a fancier solution that Simon came up with, noticing that 7 chars -> 0-127 and
+/// 3 chars -> 0-7 matched 7b111_1111 == 127 and 3b111 == 3
+///
+/// This means each "take the upper half" of a range can be converted to a binary 1 and each "take
+/// the lower half" can be a binary 0.
+fn simon_solution(input: &str) -> usize {
+    let result = input.chars().fold((0, 0), |(value, idx), c| {
+        (
+            value << 1
+                | match c {
+                    'F' | 'L' => 0,
+                    'B' | 'R' => 1,
+                    _ => panic!("Invalid char: {}", c),
+                },
+            idx + 1,
+        )
+    });
+
+    result.0
+}
+
 fn calculate_seat_id(input: &str) -> usize {
-    let row = binary_space_partition(&input[0..7], (0, 127));
-    let col = binary_space_partition(&input[7..10], (0, 7));
+    let row = simon_solution(&input[0..7]);
+    let col = simon_solution(&input[7..10]);
 
     row * 8 + col
 }
@@ -128,6 +159,30 @@ fn test_col_parse() {
     assert_eq!(7, binary_space_partition("RRR", (0, 7)));
     assert_eq!(7, binary_space_partition("RRR", (0, 7)));
     assert_eq!(4, binary_space_partition("RLL", (0, 7)));
+}
+
+#[test]
+fn test_simon_solution_same_as_naive_bsp() {
+    assert_eq!(
+        binary_space_partition("FBFBBFF", (0, 127)),
+        simon_solution("FBFBBFF")
+    );
+    assert_eq!(
+        binary_space_partition("BFFFBBF", (0, 127)),
+        simon_solution("BFFFBBF")
+    );
+    assert_eq!(
+        binary_space_partition("FFFBBBF", (0, 127)),
+        simon_solution("FFFBBBF")
+    );
+    assert_eq!(
+        binary_space_partition("BBFFBBF", (0, 127)),
+        simon_solution("BBFFBBF")
+    );
+    assert_eq!(binary_space_partition("RLR", (0, 7)), simon_solution("RLR"));
+    assert_eq!(binary_space_partition("RRR", (0, 7)), simon_solution("RRR"));
+    assert_eq!(binary_space_partition("RRR", (0, 7)), simon_solution("RRR"));
+    assert_eq!(binary_space_partition("RLL", (0, 7)), simon_solution("RLL"));
 }
 
 #[test]
