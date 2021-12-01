20928db3e610d778b43ad9aaaca06850a5be92ab
Nick Palmer <nick@palmr.co.uk>
2021-12-01 20:01:27 +0000

    2021-day1

---
+++ src/aoc2021/day_template.rs
@@ -0,0 +1,31 @@
+///
+
+pub fn solve_part_one(_input: &[String]) -> usize {
+    0
+}
+
+pub fn solve_part_two(_input: &[String]) -> usize {
+    0
+}
+
+#[test]
+fn examples_part_one() {
+    assert_eq!(
+        1,
+        solve_part_one(&[
+            "VAL_1".to_string(),
+            "VAL_2".to_string(),
+        ])
+    );
+}
+
+#[test]
+fn examples_part_two() {
+    assert_eq!(
+        1,
+        solve_part_two(&[
+            "VAL_1".to_string(),
+            "VAL_2".to_string(),
+        ])
+    );
+}
--- src/aoc2021/mod.rs
+++ src/aoc2021/mod.rs
@@ -1,5 +1,12 @@
-// use util;
+use util;
+mod day1;
 
 pub fn aoc2021() {
     println!("Advent of Code 2021");
+
+    println!(
+        "\tDay 1: Part One={}, Part Two={}",
+        day1::solve_part_one(&util::read_file_input("resources/2021/day1.txt")),
+        day1::solve_part_two(&util::read_file_input("resources/2021/day1.txt")),
+    );
 }
