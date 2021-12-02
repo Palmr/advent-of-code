b2dde88d41798079c6eed59b56feb9bab633847a
Nick Palmer <nick@palmr.co.uk>
2021-12-02 16:45:16 +0000

    2021-day2

--- src/aoc2021/mod.rs
+++ src/aoc2021/mod.rs
@@ -1,5 +1,6 @@
 use util;
 mod day1;
+mod day2;
 
 pub fn aoc2021() {
     println!("Advent of Code 2021");
@@ -8,5 +9,11 @@ pub fn aoc2021() {
         "\tDay 1: Part One={}, Part Two={}",
         day1::solve_part_one(&util::read_file_input("resources/2021/day1.txt")),
         day1::solve_part_two(&util::read_file_input("resources/2021/day1.txt")),
+    );
+
+    println!(
+        "\tDay 2: Part One={}, Part Two={}",
+        day2::solve_part_one(&util::read_file_input("resources/2021/day2.txt")),
+        day2::solve_part_two(&util::read_file_input("resources/2021/day2.txt")),
     );
 }
