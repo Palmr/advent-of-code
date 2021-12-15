18644ffe10d307281f43e9a161e55950a8c7a64f
Nick Palmer <nick@palmr.co.uk>
2021-12-15 00:16:57 +0000

    2021-day7

--- src/aoc2021/mod.rs
+++ src/aoc2021/mod.rs
@@ -5,6 +5,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 pub fn aoc2021() {
     println!("Advent of Code 2021");
@@ -43,5 +44,11 @@ pub fn aoc2021() {
         "\tDay 6: Part One={}, Part Two={}",
         day6::solve_part_one(&util::read_file_input("resources/2021/day6.txt")),
         day6::solve_part_two(&util::read_file_input("resources/2021/day6.txt")),
+    );
+
+    println!(
+        "\tDay 7: Part One={}, Part Two={}",
+        day7::solve_part_one(&util::read_file_input("resources/2021/day7.txt")),
+        day7::solve_part_two(&util::read_file_input("resources/2021/day7.txt")),
     );
 }
